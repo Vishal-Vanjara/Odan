dbb005485c60c1b2aa8ab317d4a2bf8b41e1786c
Vishal Vanjara <Vishalvanjara2370@gmail.com>
2025-09-19 12:34:05 +0530

    Day10 Work

--- day_8/lib/SecondScreen.dart
+++ day_8/lib/SecondScreen.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 
 class Secondscreen extends StatelessWidget {
 
-  final String name;
+  //Passing data Whole process done through 6 step 3, for sending data and next 3 step for receving data//
 
-  const Secondscreen({Key? key, required this.name}) : super(key: key);
+  final String name; // Step 1: Declare Variable
+  final String field;
+  final String email;
 
-  // Secondscreen(this.name);
 
-  // Secondscreen(this.nameFromHome);
+  //Step 2 :Help to receive data from other class
+  const Secondscreen({Key? key, required this.name, required this.field, required this.email}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,9 +19,30 @@ class Secondscreen extends StatelessWidget {
 
       ),
       body: Container(
-        child: Text("Welcome ${name}",),
-        alignment: Alignment.center,
-        color: Colors.orangeAccent,
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            Container(
+              child: Text("Welcome ${name}",), //Step 3 : To display
+              alignment: Alignment.center,
+              color: Colors.orangeAccent,
+            ),
+
+            Container(
+              child: Text("Your Department was ${field}",),
+              alignment: Alignment.center,
+              color: Colors.orangeAccent,
+            ),
+
+            Container(
+              child: Text("Your Email was ${email}",),
+              alignment: Alignment.center,
+              color: Colors.orangeAccent,
+            ),
+          ],
+        ),
       ),
     );
   }
